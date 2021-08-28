7b24147266acdba419df8fc8b18b7f4c4f60588b
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 12:33:50 +0530

    Added: Hello world program

---
+++ HelloWorld/Laptop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Laptop
+    {
+        String name;
+        float size;
+        public static void Run()
+        {
+            Console.WriteLine("Running");
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{ /// <summary>
+/// Testing hello world sample project
+/// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test();
+            Laptop laptop = new Laptop();
+            Laptop.Run();
+            Console.Read();
+        }
+        public static void Test()
+        {
+            Console.WriteLine("Testing");
+        }
+    }
+}
